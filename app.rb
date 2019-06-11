42edda4630064d2fb5b11b150e422bc1baf9b96c
Student <student@MAKERSs-MacBook-Pro.local>
2019-06-11 15:04:44 +0100

    applied shotgun'

--- app.rb
+++ app.rb
@@ -3,3 +3,15 @@ require 'sinatra'
 get '/' do
   'hello!'
 end
+
+get '/secret' do
+  'Who wants to know?'
+end
+
+get '/friends' do
+  'Central Perk'
+end
+
+get '/secret/please' do
+  'okay then friend'
+end
